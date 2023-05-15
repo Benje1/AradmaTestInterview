fc44f6b0c1206e93fec1e7e055330472878960ac
Benje1 <benjaminsal92@gmail.com>
2023-05-15 16:58:20 +0100

    Added wining conditions

--- AdarmaTest.js
+++ AdarmaTest.js
@@ -1,5 +1,9 @@
 function TicTacToe(){
     let board = [["-","-","-"],["-","-","-"],["-","-","-"]];
+
+    let winner = "-";
+    let winningCount = 0;
+  
     let cricle = "O";
     let cross = "X";
 
@@ -12,12 +16,135 @@ function TicTacToe(){
     }
 
     function makeAMove(indexA, indexB, symbole){
-        if(spaceCheck(indexA,indexB)){
-            board[indexA, indexB] = symbole;
+      if(spaceCheck(indexA,indexB)){
+        board[indexA][indexB] = symbole;
+      }
+    }
+    
+    function winningCondition(){
+      for(let i = 0; i < 3; i++){
+        for(let j = 0; j < 2; j++){
+          if(board[i][j] === board[i][j+1] && board[i][j] !== "-"){
+            winningCount += 1;
+            if(winningCount === 2){
+              winner = board[i][j];
+              break;
+            }
+          }
         }
+      }
+      winningCount = 0
+      for(let i = 0; i < 3; i++){
+        for(let j = 0; j < 2; j++){
+          if(board[j][i] === board[j+1][i] && board[j][i] !== "-"){
+            winningCount += 1;
+            if(winningCount === 2){
+              winner = board[j][i];
+              break;
+            }
+          }
+        }
+      }
+      winningCount = 0
+      for(let i = 0; i < 2; i++){
+        if(board[i][i] === board[i+1][i+1] && board[i][i] !== "-"){
+          winningCount += 1;
+          if(winningCount === 2){
+            winner = board[i][j];
+            break;
+          }
+        }
+      }
+      winningCount = 0
+      for(let i = 0; i< 2; i++){
+        let index = board.length - i - 1;
+        if(board[index][i] === board[index - 1][i]){
+          winningCount += 1;
+          if(winningCount === 2){
+            winner = board[index][i];
+            break;
+          }
+        }
+      }
+      winningCount = 0
     }
 
+    function clearBoard(){
+      board = [["-","-","-"],["-","-","-"],["-","-","-"]]
+    }
 
-    console.log(board[0],board[1],board[2])
+    function declareWinner(){
+      console.log("Winner is " + winner)
+    }
+
+    // makeAMove(0,0, cricle)
+    // makeAMove(0,1, cricle)
+    // makeAMove(0,2, cricle)
+    // winningCondition()
+    // if(winner !== "-"){
+    //   declareWinner()
+    // }
+    // for(let i =0; i <= 2; i++){
+    //   console.log(board[i])
+    // }
+    // clearBoard()
+    // makeAMove(0,0, cricle)
+    // makeAMove(1,0, cricle)
+    // makeAMove(2,0, cricle)
+    // winningCondition()
+    // if(winner !== "-"){
+    //   declareWinner()
+    // }
+    // for(let i =0; i <= 2; i++){
+    //   console.log(board[i])
+    // }
+    // clearBoard()
+    // makeAMove(0,0, cricle)
+    // makeAMove(1,1, cricle)
+    // makeAMove(2,2, cricle)
+    // winningCondition()
+    // if(winner !== "-"){
+    //   declareWinner()
+    // }
+    // for(let i =0; i <= 2; i++){
+    //   console.log(board[i])
+    // }
+    // clearBoard()
+    // makeAMove(0,0, cross)
+    // makeAMove(1,1, cricle)
+    // makeAMove(2,2, cross)
+    // makeAMove(0,1, cricle)
+    // makeAMove(2,1, cross)
+    // makeAMove(2,0, cricle)
+    // makeAMove(0,2, cross)
+    // makeAMove(1,2, cricle)
+    // makeAMove(1,0, cross)
+    // winningCondition()
+    // if(winner !== "-"){
+    //   declareWinner()
+    // }
+    // for(let i =0; i <= 2; i++){
+    //   console.log(board[i])
+    // }
+    // clearBoard()
+    
+    makeAMove(0,0, cross)
+    makeAMove(0,2, cricle)
+    makeAMove(2,2, cross)
+    makeAMove(1,1, cricle)
+    makeAMove(2,0, cross)
+    makeAMove(0,1, cricle)
+    makeAMove(2,1, cross)
+    winningCondition()
+    if(winner !== "-"){
+      declareWinner()
+    }
+    for(let i =0; i <= 2; i++){
+      console.log(board[i])
+    }
+    clearBoard()
 
 }
+
+
+TicTacToe()
